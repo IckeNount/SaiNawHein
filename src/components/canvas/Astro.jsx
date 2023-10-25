27acfaca737f7fe7583e2ab1783119d25ace2983
Aike Nount <aikenount@Aikes-MacBook-Air.local>
2023-10-26 05:40:24 +0630

    Deployed website

--- src/components/canvas/Astro.jsx
+++ src/components/canvas/Astro.jsx
@@ -1,79 +1,30 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { Canvas, useLoader, useFrame } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import * as THREE from 'three';
 
 import CanvasLoader from '../Loader';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const Astro = ({ isMobile }) => {
-  const astro = useLoader(GLTFLoader, './Astro/scene.gltf');
-  let mixer;
-  if (astro.animations.length) {
-    mixer = new THREE.AnimationMixer(astro.scene);
-    astro.animations.forEach(clip => {
-      const action = mixer.clipAction(clip);
-      action.play();
-    });
-  }
-
-  useFrame((state, delta) => {
-    mixer?.update(delta);
-  });
+const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf');
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={astro.scene}
-        scale={isMobile ? 2.5 : 2}
-        position={isMobile ? [0, -3.5, -2.2] : [0, -3.25, -1.5]}
-        rotation={[0, 0, 0]}
-      />
-    </mesh>
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
 const AstroCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = event => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener('change', handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener('change', handleMediaQueryChange);
-    };
-  }, []);
-
   return (
     <Canvas
-      frameloop='demand'
       shadows
+      frameloop='demand'
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -82,10 +33,10 @@ const AstroCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Astro isMobile={isMobile} />
-      </Suspense>
+        <Earth />
 
-      <Preload all />
+        <Preload all />
+      </Suspense>
     </Canvas>
   );
 };
