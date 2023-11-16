8de3f3da960a9e028150529d17603e5a3ca0723d
Aike Nount <aikenount@Aikes-MacBook-Air.local>
2023-11-16 20:58:38 +0630

    Deployed website

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,7 @@ import aronic from './company/aronic.svg';
 import aerolens from './aerolens.png';
 import noize from './noize.png';
 
-import ResumeDocx from './resume.docx';
+import ResumeDocx from './Resume.pdf';
 
 export {
   figma,
